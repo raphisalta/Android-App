9394b116d58ac7a45f3cb1d9b09d5cfbee609609
raphisalta <lalalarudisha@gmail.com>
2017-11-14 20:45:57 +0900

    --amend

--- app/src/main/java/net/nonejara/myapplication/MainActivity.kt
+++ app/src/main/java/net/nonejara/myapplication/MainActivity.kt
@@ -2,8 +2,15 @@ package net.nonejara.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import net.nonejara.myapplication.R.id.editText
+import net.nonejara.myapplication.R.id.textView
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<View>(R.id.textview) as TextView
-        textView.text = "hello works"
+        val textView = findViewById<View>(R.id.textView) as TextView
+        textView.text = "問題：[a,b,c,d,e,f,g,h]"
+
+
+        val button: Button = findViewById<View>(R.id.button) as Button
+        button.setOnClickListener {
+            val index = editText.text.toString()
+            Log.e("input number is ", index + "")
+        }
     }
 }
---
+++ app/src/main/java/net/nonejara/myapplication/arrayShift.kt
@@ -0,0 +1,12 @@
+package net.nonejara.myapplication
+
+/**
+ * Created by works on 2017/11/14.
+ */
+
+class arrayShift{
+    val array = listOf(1,2,3,4,5,6,7)
+    fun shift(index:Int):String{
+        return "" + array[1]
+    }
+}
