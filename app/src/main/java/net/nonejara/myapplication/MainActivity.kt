a34943301d9acbe96c26a8d66e331212dd06f7f4
raphisalta <lalalarudisha@gmail.com>
2017-11-14 15:25:47 +0900

    change text to hello works

--- app/src/main/java/net/nonejara/myapplication/MainActivity.kt
+++ app/src/main/java/net/nonejara/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package net.nonejara.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<View>(R.id.textview) as TextView
+        textView.text = "hello works"
     }
 }
