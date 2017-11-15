e4ac8608bf8594e39e6b7aae8b6f0327f820e8e5
raphisalta <lalalarudisha@gmail.com>
2017-11-15 19:14:48 +0900

    two page create yet complete

--- app/src/main/java/net/nonejara/myapplication/arrayShift.kt
+++ app/src/main/java/net/nonejara/myapplication/arrayShift.kt
@@ -1,12 +1,16 @@
 package net.nonejara.myapplication
 
+import android.app.Activity
+import android.util.Log
+
 /**
  * Created by works on 2017/11/14.
  */
 
 class arrayShift{
-    val array = listOf(1,2,3,4,5,6,7)
+    val array = listOf(-3,-2,3,4,5,6,7)
     fun shift(index:Int):String{
+
         return "" + array[1]
     }
 }
